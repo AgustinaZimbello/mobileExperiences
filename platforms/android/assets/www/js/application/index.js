7fe2e7cf03e68fe53647d07b8a2b45ddec7d8a31
Agustina <agustinazimbello@egmail.com>
2018-06-26 20:12:04 -0300

    cambios

--- platforms/android/assets/www/js/application/index.js
+++ platforms/android/assets/www/js/application/index.js
@@ -1,19 +1,46 @@
 var ready;
+var canvas;
 function phonegapReady() {
 	ready = true;
+
+
 }
 
 $(function() {
 	$('#createElementPositioned').click(function() {
 		if (ready) {
-			localStorage.setItem("selection", "elementPositioned");
-			window.open('maps/mapSelection.html','_blank', 'location=yes');	
+                window.canvas2ImagePlugin.saveImageDataToLibrary(
+                    function(msg){
+                        console.log(msg);
+                    },
+                    function(err){
+                        console.log(err);
+                    },
+                    canvas
+            );			// localStorage.setItem("selection", "elementPositioned");
+			// window.open('maps/mapSelection.html','_blank', 'location=yes');
 		}
 	});
 	$('#createContents').click(function() {
 		if (ready) {
-			localStorage.setItem("selection", "content");
-			window.open('content/createContent.html','_blank', 'location=yes');
+            $('#target').Jcrop({
+                onChange : updatePreview,
+                onSelect : updatePreview,
+                aspectRatio : 1
+            });
+
+            function updatePreview(c) {
+                if(parseInt(c.w) > 0) {
+                    // Show image preview
+                    var imageObj = $("#target")[0];
+                    canvas = $("#preview")[0];
+                    var context = canvas.getContext("2d");
+                    context.drawImage(imageObj, c.x, c.y, c.w, c.h, 0, 0, canvas.width, canvas.height);
+                    Canvas2Image.saveAsPNG(canvas);
+                }
+            };
+		    localStorage.setItem("selection", "content");
+			//window.open('content/createContent.html','_blank', 'location=yes');
 		}
 	});
 	$('#createPosition').click(function() {
@@ -27,6 +54,7 @@ $(function() {
 			window.open('templates/templates.html','_blank', 'location=yes');
 		}
 	});
-    
-    
+
+
+
 });
